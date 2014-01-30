241ad870f9d4caf53f893669c7b47b8ae2504138
roshow <rolando@roshow.net>
2014-01-29 23:12:10 -0500

    Procfile and app.js cleanup

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 var express = require('express'),
-    app = express(),
-    _port = 7890;
+    app = express();
 
 app.use(express.static(__dirname + '/www'));
 
-app.listen(_port);
-app.get('/hello', function(req, res) {
-    console.log('handling /');
-    res.send('hello');
-});
+app.listen(process.env.PORT || 7890);
+
 app.get('*', function(req, res) {
     console.log('handling *');
     res.render('index.jade');
